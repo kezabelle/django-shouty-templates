1dceb45ac6c7abf8331a1b38b75d89f273d32b3b
Keryn Knight <keryn@kerynknight.com>
2019-03-18 16:02:16 +0000

    packaging

---
+++ setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import os
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+if sys.version_info[0] == 2:
+    # get the Py3K compatible `encoding=` for opening files.
+	from io import open
+
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def make_readme(root_path):
+    consider_files = ("README.rst", "LICENSE", "CHANGELOG", "CONTRIBUTORS")
+    for filename in consider_files:
+        filepath = os.path.realpath(os.path.join(root_path, filename))
+        if os.path.isfile(filepath):
+            with open(filepath, mode="r", encoding="utf-8") as f:
+                yield f.read()
+
+LICENSE = "BSD License"
+URL = ""
+LONG_DESCRIPTION = "\r\n\r\n----\r\n\r\n".join(make_readme(HERE))
+SHORT_DESCRIPTION = ""
+KEYWORDS = (
+    "django",
+    "templates",
+    "DTL",
+    "exception",
+)
+
+setup(
+    name="django-shouty-templates",
+    version="0.1.0",
+    author="Keryn Knight",
+    author_email="django-shouty-templates@kerynknight.com",
+    maintainer="Keryn Knight",
+    maintainer_email="django-shouty-templates@kerynknight.com",
+    description=SHORT_DESCRIPTION[0:200],
+    long_description=LONG_DESCRIPTION,
+    packages=[],
+    py_modules=["shouty"],
+    include_package_data=True,
+    install_requires=[
+        "Django>=1.9",
+    ],
+    zip_safe=False,
+    keywords=" ".join(KEYWORDS),
+    license=LICENSE,
+    url=URL,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: {}".format(LICENSE),
+        "Natural Language :: English",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Framework :: Django",
+    ],
+)
